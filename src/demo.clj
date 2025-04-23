173a23f403baaa9c7ffba9ab259d225bde586f37
ire <ivar.refsdal@sikt.no>
2025-04-23 11:04:00 +0200

    Demo multiconsumer queue

--- src/demo.clj
+++ src/demo.clj
@@ -1,15 +1,46 @@
 (ns demo
   (:require [datomic.api :as d]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp])
+  (:import (datomic Datom)
+           (java.util.concurrent BlockingQueue LinkedBlockingQueue TimeUnit)))
 
 ; Code from:
 ; https://docs.datomic.com/peer-tutorial/peer-tutorial.html
 
-(def db-uri "datomic:dev://localhost:4334/hello")
+(set! *warn-on-reflection* true)
 
-(d/create-database db-uri)
+(def db-uri "datomic:sql://app?jdbc:sqlite:./transactor/storage/sqlite.db")
 
-(def conn (d/connect db-uri))
+(def conn
+  (do
+    (d/delete-database db-uri)
+    (d/create-database db-uri)
+    (d/connect db-uri)))
+
+(defn multicast-queue!
+  [conn consumer-ids]
+  (assert (vector? consumer-ids))
+  (assert (pos-int? (count consumer-ids)))
+  (let [multi-queues (mapv (fn [_] (LinkedBlockingQueue.)) consumer-ids)
+        running? (atom true)
+        push-counter (atom 0)
+        input-queue (d/tx-report-queue conn)
+        fut (future
+              (while @running?
+                (let [element (.poll ^BlockingQueue input-queue 1 TimeUnit/SECONDS)]
+                  (when (some? element)
+                    (doseq [q multi-queues]
+                      (.put ^BlockingQueue q element))
+                    (swap! push-counter inc))))
+             :push-thread-done)
+        close-fn (fn []
+                   (do
+                     (d/remove-tx-report-queue conn)
+                     (reset! running? false)
+                     @fut))]
+    (with-meta (zipmap consumer-ids multi-queues)
+               {:push-counter push-counter
+                :close! close-fn})))
 
 (def movie-schema [{:db/ident       :movie/title
                     :db/valueType   :db.type/string
@@ -37,22 +68,56 @@
                     :movie/genre        "punk dystopia"
                     :movie/release-year 1984}])
 
-
-
 (def all-movies-q '[:find [(pull ?e [*]) ...]               ; https://docs.datomic.com/query/query-data-reference.html#find-specs
                     :where [?e :movie/title]])
 
-(defn demo [_]
-  (println "Starting")
+(defn get-tx-data [elem]
+  (assert (some? (get elem :tx-data)))
+  (->> (get elem :tx-data)
+       (drop 1)
+       (mapv (fn [^Datom dtm]
+               [(.e dtm) (.a dtm) (.v dtm) (.tx dtm) (.added dtm)]))
+       (sort)
+       (vec)))
+
+(def tx-data-expected
+  [[17592186045418 72 "The Goonies" 13194139534313 true]
+   [17592186045418 73 "action/adventure" 13194139534313 true]
+   [17592186045418 74 1985 13194139534313 true]
+   [17592186045419 72 "Commando" 13194139534313 true]
+   [17592186045419 73 "action/adventure" 13194139534313 true]
+   [17592186045419 74 1985 13194139534313 true]
+   [17592186045420 72 "Repo Man" 13194139534313 true]
+   [17592186045420 73 "punk dystopia" 13194139534313 true]
+   [17592186045420 74 1984 13194139534313 true]])
+
+(defn demo-unicast [_]
+  (println "Starting unicast")
   @(d/transact conn movie-schema)
-  @(d/transact conn first-movies)
-  (let [db (d/db conn)]
-    (pp/pprint (d/q all-movies-q db)))
-  (println "Exiting"))
+  (let [q (d/tx-report-queue conn)]
+    @(d/transact conn first-movies)
+    (let [element (.poll ^BlockingQueue q 3 TimeUnit/SECONDS)]
+      (assert (not (nil? element)))
+      (assert (= tx-data-expected (get-tx-data element)))))
+  (println "Done unicast"))
+
+(defn demo-multicast [_]
+  (println "Starting multicast")
+  @(d/transact conn movie-schema)
+  (let [{:keys [q1 q2] :as qs} (multicast-queue! conn [:q1 :q2])
+        {:keys [close!]} (meta qs)]
+    @(d/transact conn first-movies)
+    (let [elem-1 (.poll ^BlockingQueue q1 3 TimeUnit/SECONDS)
+          elem-2 (.poll ^BlockingQueue q2 3 TimeUnit/SECONDS)]
+      (assert (some? elem-1))
+      (assert (some? elem-2))
+      (assert (= tx-data-expected (get-tx-data elem-1)))
+      (assert (= tx-data-expected (get-tx-data elem-2)))
+      (assert (= :push-thread-done (close!)))))
+  (println "Done multicast"))
 
 (defn read-only [_]
   (println "Starting")
   (let [db (d/db conn)]
     (pp/pprint (d/q all-movies-q db)))
   (println "Exiting"))
-
